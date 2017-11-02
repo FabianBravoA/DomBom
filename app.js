188c45c17d9e3e4f62b9802ae27a243c123d541f
Fabián Alexis Bravo Abarca <fabian.bravo.abarca@gmail.com>
2017-11-02 11:43:38 -0300

    Validacion completa

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
 window.onload = function() {
     var input = window.document.getElementById("ingresa");
-    input.onkeypress = function() {
+    input.onkeyup = function() {
         var text = input.value;
         //Javascript es maldito y no quizo == o ===
         if (isNaN(parseInt(text))) {
             var pAlert = document.createElement("p");
+            pAlert.id = "alerta";
             pAlert.innerText = "No es un número";
+
+            //Checkear que sea el unico "alerta"
             var inputDiv = document.getElementById("inputDiv");
+            var posibleAlert = document.getElementById("alerta");
+            if (posibleAlert) {
+                inputDiv.removeChild(posibleAlert);
+            }
             inputDiv.appendChild(pAlert);
         } else {
-            console.log("Es un número");
+            //Checkear que sea el unico "alerta"
+            var inputDiv = document.getElementById("inputDiv");
+            var posibleAlert = document.getElementById("alerta");
+            if (posibleAlert) {
+                inputDiv.removeChild(posibleAlert);
+            }
         }
     }
 }
