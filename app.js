dd2da03fca077d8833f725949f17e47fc8e088f6
Fabián Alexis Bravo Abarca <fabian.bravo.abarca@gmail.com>
2017-11-02 11:26:29 -0300

    Validación básica

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 window.onload = function() {
     var input = window.document.getElementById("ingresa");
     input.onkeypress = function() {
-        console.log("Me cambiaron > " + input.value);
+        var text = input.value;
+        //Javascript es maldito y no quizo == o ===
+        if (isNaN(parseInt(text))) {
+            console.log("No es un número");
+        } else {
+            console.log("Es un número");
+        }
     }
 }
